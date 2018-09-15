8799132b2b3545893ba7eb9cc77960ac5949da44
jimboLix <lrhtomcat@163.com>
2018-09-15 09:58:22 +0800

    1

--- src/main/java/com/jimbolix/support/config/McvConfiger.java
+++ src/main/java/com/jimbolix/support/config/McvConfiger.java
@@ -1,10 +1,14 @@
 package com.jimbolix.support.config;
 
+import com.jimbolix.support.componet.CustomLocaleResolver;
 import com.jimbolix.support.interceptor.CustomHandlerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -22,4 +26,10 @@ public class McvConfiger implements WebMvcConfigurer{
         .addPathPatterns("/index/*")
         ;
     }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+    }
+
 }
