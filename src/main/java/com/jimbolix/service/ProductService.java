6e25e6bc1afcd943515c0eb2e2c4b8b562aaedfc
jimboLix <lrhtomcat@163.com>
2018-09-19 07:59:18 +0800

    添加Sl4j的日志配置

---
+++ src/main/java/com/jimbolix/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.jimbolix.service;
+
+/**
+ * @author ruihui.li
+ * @version V1.0
+ * @Title: springboot
+ * @Package com.jimbolix.service
+ * @Description: ${todo}(用一句话描述该文件做什么)
+ * @date 2018/9/19
+ */
+public interface ProductService {
+}
